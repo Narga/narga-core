24c6803c47f1fa83557e1063bf87882eab7acb96
Nguyễn Đình Quân <dinhquan@narga.net>
2012-12-17 14:19:04 +0700

    Clean unused codes, update full width templates and page html code

--- page.php
+++ page.php
@@ -1,14 +1,9 @@
 <?php get_header(); ?>
-
-		<!-- Row for main content area -->
-		<div id="content" class="eight columns">
-	
-			<div class="post-box">
-				<?php get_template_part('loop', 'page'); ?>
-			</div>
-
-		</div><!-- End Content row -->
-		
-		<?php get_sidebar(); ?>
-		
+    <!-- Row for main content area -->
+    <div id="content" class="eight columns page-content-wrapper">
+    <div class="post-box">
+    <?php get_template_part('loop', 'page'); ?>
+    </div>
+    </div><!-- End Content row -->
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- templates/full-width.php
+++ templates/full-width.php
@@ -3,15 +3,10 @@
 Template Name: Full Width
 */
 get_header(); ?>
-
-		<!-- Row for main content area -->
-		<div id="content" class="twelve columns">
-	
-			<div class="post-box">
-				<?php get_template_part('loop', 'page'); ?>
-			</div>
-
-		</div><!-- End Content row -->
-		
-		
+    <!-- Row for main content area -->
+    <div id="content" class="twelve columns full-width-content-wrapper">
+    <div class="post-box">
+    <?php get_template_part('loop', 'page'); ?>
+    </div>
+    </div><!-- End Content row -->
 <?php get_footer(); ?>
