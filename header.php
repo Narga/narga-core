8216f3e49435a999911a3e551fd96aa5480cdb50
Nguyễn Đình Quân <dinhquan@narga.net>
2014-02-18 15:43:53 +0700

    Allow user can insert custom codes in header

--- header.php
+++ header.php
@@ -29,7 +29,12 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <!-- Mobile viewport optimized: j.mp/bplateviewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <?php wp_head(); ?>
+<?php
+if (narga_options('custom_header_codes') != '') :
+    echo narga_options('custom_header_codes');
+endif;
+wp_head();
+?>
 </head>
 <body <?php body_class(); ?>>
 
