fcd0225ecf02cb873401b04de3684e3c4bc3eba7
Nguyễn Đình Quân <dinhquan@narga.net>
2014-08-27 16:34:07 +0700

    Remove Pingback: prefix at the begining of pingbacks list

--- library/layouts/custom-content.php
+++ library/layouts/custom-content.php
@@ -87,7 +87,7 @@ if (!function_exists('narga_comments')) :
                 // Display trackbacks differently than normal comments.
         ?>
         <li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
-                <p><?php _e( 'Pingback:', 'narga' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'narga' ), '<span class="edit-link">', '</span>' ); ?></p>
+               <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'narga' ), '<span class="edit-link">', '</span>' ); ?>
 <?php
                     break;
                 default :
